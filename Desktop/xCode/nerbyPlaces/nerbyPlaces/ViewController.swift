a32b487b1f556fcb60b5cb82a396608437470f94
Martin Michalko <matko1711@gmail.com>
2022-03-16 00:32:01 -0400

    Nearby Places Full Project

---
+++ Desktop/xCode/nerbyPlaces/nerbyPlaces/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  nerbyPlaces
+//
+//  Created by Martin Michalko on 23/10/2017.
+//  Copyright © 2017 Martin Michalko. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+import GooglePlaces
+
+class ViewController: UIViewController {
+    
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var mapView: GMSMapView!
+    var placesClient: GMSPlacesClient!
+    var zoomLevel: Float = 15.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 10.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
+        
+    }
+}
+
